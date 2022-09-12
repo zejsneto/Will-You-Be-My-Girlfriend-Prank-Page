35765a47046514b91bee789055a8a292f283e1fd
José Joaquim da Silva Neto <zeneto2508@gmail.com>
2022-09-12 14:53:45 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+count = 0;
+
+$("#change").click(function() {
+    $("#change").html("<a onclick='gotIt()'> Yes </a>");   
+    $("#move").show();
+    if (count == 1) {
+        gotIt();
+    }
+    count = 1;
+  });
+
+  function gotIt() {
+    alert("Love u!");
+}
+  
+  $("#move").hover(function() {
+    change();
+  });
+  
+  function change(){
+      var i = Math.floor(Math.random()*1000)+1;
+      var j = Math.floor(Math.random()*1000)+1;
+      $("#move").css({top: j+'px',left: i+'px'});
+  }
